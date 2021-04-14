5392809b7244b2be1093481c43238cc642bc583b
Herney Ruiz <herneyruiz36@gmail.com>
2021-04-13 23:50:23 -0500

    test: User can store product

--- tests/Feature/Product/ProductControllerTest.php
+++ tests/Feature/Product/ProductControllerTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature\Product;
 
+use App\Models\Brand;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class ProductControllerTest extends TestCase
@@ -66,5 +69,50 @@ class ProductControllerTest extends TestCase
             ->get(route('products.create'))
             ->assertStatus(200)
             ->assertViewIs("products.create");
+    }
+
+        /**
+     * @return void
+     * @test
+     */
+    public function an_unauthenticated_user_cannot_create_products()
+    {
+        $this->post(route('products.store'), [])
+        ->assertStatus(302)
+        ->assertRedirect("login");
+    }
+
+    /**
+     * @return void
+     * @test
+     */
+    public function an_authenticated_user_can_create_products()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $brand = Brand::factory()->create();
+
+        $data = [
+            "name"              => "iPhone 11",
+            "size"              => "L",
+            "observations"      => "El iPhone 11 refleja el compromiso continuo de Apple con el medio ambiente.",
+            "stock"             => 10,
+            "shipment"          => Date::now(),
+            "brand_id"          => $brand->id,
+        ];
+
+        $this
+            ->post(route('products.store'), $data)
+            ->assertStatus(302)
+            ->assertRedirect(route("products.index"));
+
+        $this->assertDatabaseHas('products', [
+            "name"      => $data["name"],
+            "size"      => $data["size"],
+            "stock"     => $data["stock"],
+        ]);
     }
 }
