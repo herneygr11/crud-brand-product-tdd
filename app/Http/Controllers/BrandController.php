4871c653a75fe73f8211f865b2e066f94b152f48
Herney Ruiz <herneyruiz36@gmail.com>
2021-04-13 19:48:00 -0500

    feat: Return all brands

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
 use Illuminate\Http\Request;
 
 class BrandController extends Controller
@@ -13,6 +14,8 @@ class BrandController extends Controller
      */
     public function index()
     {
-        return view('brands.index');
+        $brands = Brand::all();
+
+        return view('brands.index', compact('brands'));
     }
 }
