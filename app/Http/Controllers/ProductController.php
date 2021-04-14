8c29ca0e5f69c247164e9f17ea2f6b901c1719ae
Herney Ruiz <herneyruiz36@gmail.com>
2021-04-13 23:24:42 -0500

    feat: Method index and view index

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+        return view('products.index', compact('products'));
+    }
+}
