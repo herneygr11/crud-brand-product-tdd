e06ba28772efc2c807d54fb6575ac51f89fd74b1
Herney Ruiz <herneyruiz36@gmail.com>
2021-04-13 19:23:22 -0500

    test: Acting as user

--- tests/Feature/Brand/ControllerBrandTest.php
+++ tests/Feature/Brand/ControllerBrandTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Brand;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -19,6 +20,9 @@ class ControllerBrandTest extends TestCase
     public function an_authenticated_user_can_see_the_brand_index_view()
     {
         $this->withoutExceptionHandling();
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
 
         $response = $this->get(route('brands.index'));
         $response->assertStatus(200);
