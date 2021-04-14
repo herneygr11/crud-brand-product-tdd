2354dcea077c23aab772c13adc05a7e8ac2e96db
Herney Ruiz <herneyruiz36@gmail.com>
2021-04-13 23:51:22 -0500

    feat: Filds by fillable

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        "name",
+        "size",
+        "observations",
+        "stock",
+        "slug",
+        "shipment",
+        "brand_id",
+    ];
+}
