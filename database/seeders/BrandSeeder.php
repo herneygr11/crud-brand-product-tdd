0db3dea6624924d2eac861b28f9d2a4c56ae93a2
Herney Ruiz <herneyruiz36@gmail.com>
2021-04-14 00:56:13 -0500

    feat: brand seeder

---
+++ database/seeders/BrandSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Brand;
+use Illuminate\Support\Str;
+
+class BrandSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $brands = [
+            [
+                "name"  => "Apple",
+                "slug"  => Str::slug("Apple"),
+            ],
+            [
+                "name"  => "Huawei",
+                "slug"  => Str::slug("Huawei"),
+            ],
+            [
+                "name"  => "Samsung",
+                "slug"  => Str::slug("Samsung"),
+            ],
+            [
+                "name"  => "Xiaomi",
+                "slug"  => Str::slug("Xiaomi"),
+            ],
+            [
+                "name"  => "Honor",
+                "slug"  => Str::slug("Honor"),
+            ],
+        ];
+
+        echo "Creating brands items " . "\n";
+        foreach ( $brands as $brand ){
+            echo "\t" . "Creating brand " . $brand[ 'name' ] . "\n";
+            Brand::updateOrCreate(
+                [
+                    "name" => $brand[ 'name' ],
+                    "slug" => $brand[ 'slug' ],
+                ],
+                [
+                    "name" => $brand[ 'name' ],
+                    "slug" => $brand[ 'slug' ],
+                ]
+            );
+            echo "\t" . "Created brand " . $brand[ 'name' ] . "\n";
+        }
+        echo "Created brands items" . "\n";
+    }
+}
